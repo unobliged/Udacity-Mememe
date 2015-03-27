261e97de13b1598d2076a8af68b0589615f737f0
Brian <brian.ortega@gmail.com>
2015-03-27 17:35:19 -0400

    Committed by Udacity

---
+++ MemeMe/MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Brian Ortega on 3/23/15.
+//  Copyright (c) 2015 Brian Ortega. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memeCellImageView: UIImageView!
+}
