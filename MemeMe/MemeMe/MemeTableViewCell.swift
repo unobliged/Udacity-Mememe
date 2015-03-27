f11019bbea0e3290983aa23b574c1f2813d9d036
Brian <brian.ortega@gmail.com>
2015-03-27 17:35:20 -0400

    Committed by Udacity

---
+++ MemeMe/MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Brian Ortega on 3/24/15.
+//  Copyright (c) 2015 Brian Ortega. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+    @IBOutlet weak var memeTableCellImageView: UIImageView!
+    @IBOutlet weak var memeTableCellLabel: UILabel!
+}
