e4af2cf5a332fa0d0db6a6e9379c8595ba7011e4
Brian <brian.ortega@gmail.com>
2015-03-27 17:35:19 -0400

    Committed by Udacity

---
+++ MemeMe/MemeMe/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Brian Ortega on 3/23/15.
+//  Copyright (c) 2015 Brian Ortega. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var image: UIImage
+    var memedImage: UIImage
+}
+
